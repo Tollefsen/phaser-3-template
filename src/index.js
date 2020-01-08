b07794dd3181336d198ba7936260452e74572521
Håvard Tollefsen <tollefsen@Havards-MacBook-Pro.local>
2020-01-08 19:23:10 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**
+ * Author: Michael Hadley, mikewesthad.com
+ * Asset Credits:
+ *  - Subtle Patterns
+ */
+
+const config = {
+	type: Phaser.AUTO,
+	width: 171,
+	height: 160,
+	zoom: 4, // Since we're working with 16x16 pixel tiles, let's scale up the canvas by 4x
+	pixelArt: true, // Force the game to scale images up crisply
+	parent: 'game-container',
+	scene: {
+		preload: preload,
+		create: create
+	}
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+	// "this" === Phaser.Scene
+	this.load.image('tiles', '../assets/tilesets/super-mario-tiles.png');
+}
+
+function create() {
+	// Load a map from a 2D array of tile indices
+	// prettier-ignore
+	const level = [
+		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+		[  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
+		[  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
+		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+		[  0,  0,  0, 14, 13, 14,  0,  0,  0,  0,  0 ],
+		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+		[  0,  0, 14, 14, 14, 14, 14,  0,  0,  0, 15 ],
+		[  0,  0,  0,  0,  0,  0,  0,  0,  0, 15, 15 ],
+		[ 35, 36, 37,  0,  0,  0,  0,  0, 15, 15, 15 ],
+		[ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+	];
+
+	// When loading from an array, make sure to specify the tileWidth and tileHeight
+	const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
+	const tiles = map.addTilesetImage('tiles');
+	const layer = map.createStaticLayer(0, tiles, 0, 0);
+}
+
+function update(time, delta) {
+	// We aren't using this in the current example, but here is where you can run logic that you need
+	// to check over time, e.g. updating a player sprite's position based on keyboard input
+}
